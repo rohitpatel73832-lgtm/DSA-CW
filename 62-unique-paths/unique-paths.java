bf1500bfc11eae17b24a83f8dacc8ebdc8a51d37
rohitpatel73832-lgtm <rohitpatel73832@gmail.com>
2026-02-26 02:22:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (11.45%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int helper(int r,int c,int m, int n,int[][] dp){
+
+        if(r>=m || c>=n) return 0;
+
+        if(r==m-1 && c==n-1) return 1;
+
+        if(dp[r][c]!=-1) return dp[r][c];
+
+        int right = helper(r,c+1,m,n,dp);
+        int down  = helper(r+1,c,m,n,dp);
+
+        return dp[r][c] = right + down;
+    }
+
+    public int uniquePaths(int m, int n) {
+
+       int[][] dp = new int[m][n];
+
+       for(int i=0;i<m;i++){
+          for(int j=0;j<n;j++){
+            dp[i][j] = -1;
+          }
+       }
+
+       return helper(0,0,m,n,dp);
+    }
+}
