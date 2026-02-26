54377dcdb7c5a0ed457075eb1c57e050d9ec1de6
rohitpatel73832-lgtm <rohitpatel73832@gmail.com>
2026-02-27 03:08:25 +0530

    Time: 197 ms (11.04%) | Memory: 44.5 MB (75.79%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPerfect(int n){
+        int sqrt=(int)Math.sqrt(n);
+        if(sqrt*sqrt==n) return true;
+        return false;
+        
+    }
+    public int minS(int n,int[] dp){
+        if(isPerfect(n)) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int min=Integer.MAX_VALUE;
+        for(int i=1; i*i<=n;i++){
+            int count=minS(i*i,dp) +minS(n-i*i,dp);
+            min=Math.min(min,count);
+           
+        }
+         return dp[n]=min;
+    }
+    public int numSquares(int n) {
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return minS(n,dp);
+    }
+}
