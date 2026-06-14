fdab64d39d7e50f24b084b38eb7195bd5c6b00a6
Rohit Patel <rohitpatel73832@gmail.com>
2026-06-15 02:31:48 +0530

    Time: 127 ms (5.98%) | Memory: 47.5 MB (8.21%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,52 +1,41 @@
-// class Solution {
-
-//     public int helper(int i, int[] nums) {
-
-//         int n = nums.length;
-
-//         if (i >= n - 1) {
-//             return 0;
-//         }
-
-//         int ans = Integer.MAX_VALUE;
-
-//         for (int jump = 1; jump <= nums[i]; jump++) {
-
-//             int next = i + jump;
-
-//             if (next < n) {
-//                 int temp = helper(next, nums);
-
-//                 if (temp != Integer.MAX_VALUE) {
-//                     ans = Math.min(ans, 1 + temp);
-//                 }
-//             }
-//         }
-
-//         return ans;
-//     }
-
-//     public int jump(int[] nums) {
-//         return helper(0, nums);
-//     }
-// }
 class Solution {
-    public int jump(int[] nums) {
 
-        int jumps = 0;
-        int currentEnd = 0;
-        int farthest = 0;
+    public int helper(int i, int[] nums, int[] dp) {
 
-        for (int i = 0; i < nums.length - 1; i++) {
+        int n = nums.length;
 
-            farthest = Math.max(farthest, i + nums[i]);
+        if (i >= n - 1) {
+            return 0;
+        }
 
-            if (i == currentEnd) {
-                jumps++;
-                currentEnd = farthest;
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int jump = 1; jump <= nums[i]; jump++) {
+
+            int next = i + jump;
+
+            if (next < n) {
+
+                int temp = helper(next, nums, dp);
+
+                if (temp != Integer.MAX_VALUE) {
+                    ans = Math.min(ans, 1 + temp);
+                }
             }
         }
 
-        return jumps;
+        return dp[i] = ans;
+    }
+
+    public int jump(int[] nums) {
+
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+
+        return helper(0, nums, dp);
     }
 }
