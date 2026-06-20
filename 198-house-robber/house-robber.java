1d3ad1dcd69e4de673bf2cf33f0a250d571ede3e
Rohit Patel <rohitpatel73832@gmail.com>
2026-06-20 22:50:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (36.49%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,19 +1,29 @@
 class Solution {
 
-    public int amount(int[] nums, int i, int[] dp) {
-        if (i >= nums.length) return 0;
+    // public int amount(int[] nums, int i, int[] dp) {
+    //     if (i >= nums.length) return 0;
 
-        if (dp[i] != -1) return dp[i];
-        int skip = amount(nums, i+1, dp);
-        int take = nums[i] + amount(nums, i+2, dp);
+    //     if (dp[i] != -1) return dp[i];
+    //     int skip = amount(nums, i+1, dp);
+    //     int take = nums[i] + amount(nums, i+2, dp);
 
-        return dp[i] = Math.max(take, skip);
-    }
+    //     return dp[i] = Math.max(take, skip);
+    // }
 
-    public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
+     public int rob(int[] nums) {
+    //     int[] dp = new int[nums.length];
+    //     Arrays.fill(dp, -1);
 
-        return amount(nums, 0, dp);
+    //     return amount(nums, 0, dp);
+
+       int n= nums.length;
+       int[] dp= new int[n];
+       if(n==1) return nums[0];
+       dp[0]=nums[0];
+       dp[1]=Math.max(nums[0],nums[1]);
+       for(int i=2; i<n; i++){
+        dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+       }
+       return dp[n-1];
     }
 }
