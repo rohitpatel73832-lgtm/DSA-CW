4727b5c43be26d0d33c09fad224447ab091fafe0
Rohit Patel <rohitpatel73832@gmail.com>
2026-06-27 11:11:05 +0530

    Time: 1 ms (19.72%) | Memory: 46.3 MB (82.73%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -33,53 +33,66 @@
 //         return helper(0, 1, prices);
 //     }
 // }
+// class Solution {
+
+//     public int helper(int i, int canBuy, int[] prices, int[][] dp) {
+
+        
+//         if (i == prices.length) {
+//             return 0;
+//         }
+
+        
+//         if (dp[i][canBuy] != -1) {
+//             return dp[i][canBuy];
+//         }
+
+//         if (canBuy == 1) {
+
+           
+//             int buy = -prices[i] + helper(i + 1, 0, prices, dp);
+
+//             int skip = helper(i + 1, 1, prices, dp);
+
+//             dp[i][canBuy] = Math.max(buy, skip);
+
+//         } else {
+
+           
+//             int sell = prices[i] + helper(i + 1, 1, prices, dp);
+
+           
+//             int hold = helper(i + 1, 0, prices, dp);
+
+//             dp[i][canBuy] = Math.max(sell, hold);
+//         }
+
+//         return dp[i][canBuy];
+//     }
+
+//     public int maxProfit(int[] prices) {
+
+//         int n = prices.length;
+
+//         int[][] dp = new int[n][2];
+
+//         for (int i = 0; i < n; i++) {
+//             Arrays.fill(dp[i], -1);
+//         }
+
+//         return helper(0, 1, prices, dp);
+//     }
+// }
 class Solution {
-
-    public int helper(int i, int canBuy, int[] prices, int[][] dp) {
-
-        
-        if (i == prices.length) {
-            return 0;
-        }
-
-        
-        if (dp[i][canBuy] != -1) {
-            return dp[i][canBuy];
-        }
-
-        if (canBuy == 1) {
-
-           
-            int buy = -prices[i] + helper(i + 1, 0, prices, dp);
-
-            int skip = helper(i + 1, 1, prices, dp);
-
-            dp[i][canBuy] = Math.max(buy, skip);
-
-        } else {
-
-           
-            int sell = prices[i] + helper(i + 1, 1, prices, dp);
-
-           
-            int hold = helper(i + 1, 0, prices, dp);
-
-            dp[i][canBuy] = Math.max(sell, hold);
-        }
-
-        return dp[i][canBuy];
-    }
-
     public int maxProfit(int[] prices) {
+        int profit=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>prices[i-1]){
+                profit +=prices[i]-prices[i-1];
+            }
 
-        int n = prices.length;
-
-        int[][] dp = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
         }
-
-        return helper(0, 1, prices, dp);
+        return profit;
+        
     }
 }
