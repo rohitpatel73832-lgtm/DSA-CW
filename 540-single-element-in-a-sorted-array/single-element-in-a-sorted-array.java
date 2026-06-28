2d0ddea5a19ec8e922c0878c256ecd30c47cf7fc
Rohit Patel <rohitpatel73832@gmail.com>
2026-06-28 13:33:33 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (20.77%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,15 +1,38 @@
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
+//         HashMap<Integer,Integer> mp = new HashMap<>();
+//         for(int i=0;i<nums.length; i++){
+//             mp.put(nums[i],mp.getOrDefault(nums[i], 0) + 1);
+//         }
+//         for (Map.Entry<Integer, Integer> ele : mp.entrySet()) {
+//             if (ele.getValue() == 1) {
+//                 return ele.getKey();
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        HashMap<Integer,Integer> mp = new HashMap<>();
-        for(int i=0;i<nums.length; i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i], 0) + 1);
-        }
-        for (Map.Entry<Integer, Integer> ele : mp.entrySet()) {
-            if (ele.getValue() == 1) {
-                return ele.getKey();
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            } else {
+                high = mid;
             }
         }
 
-        return -1;
+        return nums[low];
     }
 }
