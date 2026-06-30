c6ddf28ef69726eb3f81d3cc5eb19adfc814fac6
Rohit Patel <rohitpatel73832@gmail.com>
2026-06-30 23:30:27 +0530

    Time: 1 ms (23.18%) | Memory: 43.1 MB (9.76%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,19 +1,75 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public boolean checkValidString(String s) {
+
+//         Stack<Integer> open = new Stack<>();
+//         Stack<Integer> star = new Stack<>();
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+
+//             if (ch == '(') {
+//                 open.push(i);
+//             }
+//             else if (ch == '*') {
+//                 star.push(i);
+//             }
+//             else { // ch == ')'
+
+//                 if (!open.isEmpty()) {
+//                     open.pop();
+//                 }
+//                 else if (!star.isEmpty()) {
+//                     star.pop();
+//                 }
+//                 else {
+//                     return false;
+//                 }
+//             }
+//         }
+
+//         while (!open.isEmpty() && !star.isEmpty()) {
+
+//             if (open.peek() < star.peek()) {
+//                 open.pop();
+//                 star.pop();
+//             } else {
+//                 return false;
+//             }
+//         }
+
+//         return open.isEmpty();
+//     }
+// }
+
 
 class Solution {
+
+    class Pair {
+        char ch;
+        int index;
+
+        Pair(char ch, int index) {
+            this.ch = ch;
+            this.index = index;
+        }
+    }
+
     public boolean checkValidString(String s) {
 
-        Stack<Integer> open = new Stack<>();
-        Stack<Integer> star = new Stack<>();
+        Stack<Pair> open = new Stack<>();
+        Stack<Pair> star = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
+
             char ch = s.charAt(i);
 
             if (ch == '(') {
-                open.push(i);
+                open.push(new Pair(ch, i));
             }
             else if (ch == '*') {
-                star.push(i);
+                star.push(new Pair(ch, i));
             }
             else { // ch == ')'
 
@@ -31,7 +87,10 @@ class Solution {
 
         while (!open.isEmpty() && !star.isEmpty()) {
 
-            if (open.peek() < star.peek()) {
+            Pair openPair = open.peek();
+            Pair starPair = star.peek();
+
+            if (openPair.index < starPair.index) {
                 open.pop();
                 star.pop();
             } else {
