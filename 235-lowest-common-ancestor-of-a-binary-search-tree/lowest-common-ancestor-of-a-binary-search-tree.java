5f2e8a9e20dd3e52b72cb34ac503a07e9ec10f4d
Rohit Patel <rohitpatel73832@gmail.com>
2026-07-11 11:18:08 +0530

    Time: 6 ms (97.20%) | Memory: 47.9 MB (16.58%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,17 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
 class Solution {
-    public boolean isContains(TreeNode root,TreeNode node){
-        if(root == null) return false;
-        if(root==node) return true;
-        return isContains(root.left,node) || isContains(root.right,node);
-    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(p==root|| q==root) return root;
-        if(p==q) return p;
-        boolean leftp= isContains(root.left,p);
-        boolean rightq=isContains(root.right,q);
-        if((leftp && rightq) || (!leftp && !rightq)) return root;
-        if(leftp && !rightq) return lowestCommonAncestor(root.left,p,q);
-        if(!leftp && rightq) return lowestCommonAncestor(root.right,p,q);
+        if(p.val==root.val || q.val==root.val) return root;
+        if(p.val<root.val && q.val>root.val) return root;
+        if(p.val>root.val && q.val<root.val) return root;
+        if(p.val<root.val && q.val<root.val) return lowestCommonAncestor(root.left,p,q);
+        if(p.val>root.val && q.val>root.val) return lowestCommonAncestor(root.right,p,q);
         return null;
+
     }
 }
