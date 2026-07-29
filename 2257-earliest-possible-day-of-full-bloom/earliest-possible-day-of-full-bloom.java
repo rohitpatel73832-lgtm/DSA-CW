4b808d2de280ee6d573e7cb0f0b0b859b76a1e50
Rohit Patel <rohitpatel73832@gmail.com>
2026-07-30 00:10:35 +0530

    Time: 51 ms (77.48%) | Memory: 126.5 MB (13.91%) - LeetSync

---
+++ 2257-earliest-possible-day-of-full-bloom/earliest-possible-day-of-full-bloom.java
@@ -0,0 +1,34 @@
+class Solution {
+    class Pair implements Comparable<Pair> {
+        int pt;
+        int gt;
+        Pair(int pt, int gt) {
+            this.pt = pt;
+            this.gt = gt;
+        }
+        public int compareTo(Pair other) {
+            return Integer.compare(other.gt, this.gt); // Descending by grow time
+        }
+    }
+
+    public int earliestFullBloom(int[] p, int[] g) {
+        List<Pair> adj = new ArrayList<>();
+        for (int i = 0; i < p.length; i++) {
+            adj.add(new Pair(p[i], g[i]));
+        }
+        Collections.sort(adj);
+        int days = 0;
+        int maxDays = 0;
+
+        for (int i = 0; i < adj.size(); i++) {
+
+            Pair curr = adj.get(i);
+
+            days += curr.pt;
+
+            maxDays = Math.max(maxDays, days + curr.gt);
+        }
+
+        return maxDays;
+    }
+}
