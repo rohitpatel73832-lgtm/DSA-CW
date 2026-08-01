c4f6df6fc25769b424d17fa913da2c3f8f7cd9e5
Rohit Patel <rohitpatel73832@gmail.com>
2026-08-01 15:47:24 +0530

    Time: 70 ms (16.33%) | Memory: 42.9 MB (41.06%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,23 +1,42 @@
-class Solution {
-    public int helper(int i, int j,int[] nums){
+// class Solution {
+//     public int helper(int i, int j,int[] nums){
+//         if(i>j){
+//             return 0;
+//         }
+//         if(i==j){
+//             return nums[i];
+//         }
+//         int takeI=nums[i]+Math.min(helper(i+2,j,nums),helper(i+1,j-1,nums));
+//         int takeJ=nums[j]+Math.min(helper(i+1,j-1,nums),helper(i,j-2,nums));
+//         return Math.max(takeI,takeJ);
+//     }
+//     public boolean predictTheWinner(int[] nums) {
+//         int n= nums.length;
+//         int totalScore=0;
+//         for(int i=0; i<n; i++){
+//             totalScore+=nums[i];
+//         }
+//         int player1Score=helper(0,n-1,nums);
+//         int player2Score=totalScore-player1Score;
+//         return player1Score>=player2Score;
+//     }
+// }
+
+class Solution{
+    public int helper(int i,int j,int[] nums){
         if(i>j){
             return 0;
         }
         if(i==j){
             return nums[i];
         }
-        int takeI=nums[i]+Math.min(helper(i+2,j,nums),helper(i+1,j-1,nums));
-        int takeJ=nums[j]+Math.min(helper(i+1,j-1,nums),helper(i,j-2,nums));
+        int takeI=nums[i]-helper(i+1,j,nums);
+        int takeJ=nums[j]-helper(i,j-1,nums);
         return Math.max(takeI,takeJ);
     }
-    public boolean predictTheWinner(int[] nums) {
-        int n= nums.length;
-        int totalScore=0;
-        for(int i=0; i<n; i++){
-            totalScore+=nums[i];
-        }
-        int player1Score=helper(0,n-1,nums);
-        int player2Score=totalScore-player1Score;
-        return player1Score>=player2Score;
+    public boolean predictTheWinner(int[] nums){
+        int n=nums.length;
+        return helper(0,n-1,nums)>=0;
     }
+
 }
