7dd2fa9482d421109eb39da7acdd2fd2b9e03508
Rohit Patel <rohitpatel73832@gmail.com>
2026-08-06 02:24:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (43.19%) - LeetSync

---
+++ 413-arithmetic-slices/arithmetic-slices.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n= nums.length;
+        int curr=0; int total=0;
+        for(int i=2; i<n; i++){
+            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
+                curr++;
+                total=total+curr;
+            }else{
+                curr=0;
+            }
+        }
+        return total;
+    }
+}
