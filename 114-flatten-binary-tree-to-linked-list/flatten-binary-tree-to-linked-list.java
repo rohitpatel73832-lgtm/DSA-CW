05e6e885ef0a6ce9da1b91dfdbdddd354741a31c
Rohit Patel <rohitpatel73832@gmail.com>
2026-08-13 23:00:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (16.92%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -1,19 +1,35 @@
 
 class Solution {
     public void flatten(TreeNode root) {
-        if(root==null) return;
-        TreeNode l=root.left;
-        TreeNode r= root.right;
-        root.left=null;
-        flatten(l);
-        flatten(r);
-        root.right=l;
-        TreeNode temp=root;
-        while(temp!=null && temp.right!=null){
-            temp=temp.right;
+        // if(root==null) return;
+        // TreeNode l=root.left;
+        // TreeNode r= root.right;
+        // root.left=null;
+        // flatten(l);
+        // flatten(r);
+        // root.right=l;
+        // TreeNode temp=root;
+        // while(temp!=null && temp.right!=null){
+        //     temp=temp.right;
+        // }
+        // if(temp!=null) temp.right=r;
+        // else root.right=r;
+        // return;
+
+        //Morris- Traversal
+
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode pred=curr.left;
+                while(pred.right!=null){
+                    pred=pred.right;
+                }
+                pred.right=curr.right;
+                curr.right=curr.left;
+                curr.left=null;
+            }
+            curr=curr.right;
         }
-        if(temp!=null) temp.right=r;
-        else root.right=r;
-        return;
     }
 }
