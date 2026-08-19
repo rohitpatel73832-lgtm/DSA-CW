a0d6ae20730efe84b787c62a2afccc85690fbf41
Rohit Patel <rohitpatel73832@gmail.com>
2026-08-20 02:53:19 +0530

    Time: 79 ms (12.32%) | Memory: 100.7 MB (97.11%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,35 +1,60 @@
+
 // class Solution {
 //     public int[] dailyTemperatures(int[] t) {
-//         int n=t.length;
+//         int n= t.length;
 //         int[] ans= new int[n];
-       
+//         Stack<Integer> st= new Stack<>();
 //         for(int i=0; i<n; i++){
-//             ans[i]=0;
-//             for(int j=i+1; j<n; j++){
-//                 if(t[j]>t[i]){
-                    
-//                     ans[i]=j-i;
-                    
-//                     break;
-//                 }
+//             while(st.size()>0 && t[i]>t[st.peek()]){
+//                 int prev= st.pop();
+//                 ans[prev]=i-prev;
 //             }
+//             st.push(i);
 //         }
 //         return ans;
 //     }
 // }
 
 class Solution {
-    public int[] dailyTemperatures(int[] t) {
-        int n= t.length;
-        int[] ans= new int[n];
-        Stack<Integer> st= new Stack<>();
-        for(int i=0; i<n; i++){
-            while(st.size()>0 && t[i]>t[st.peek()]){
-                int prev= st.pop();
-                ans[prev]=i-prev;
+
+    public int[] dailyTemperatures(int[] temp) {
+
+        int n = temp.length;
+
+        int[] nge = new int[n];
+        int[] ans = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        nge[n - 1] = 0;
+        st.push(n - 1);
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            // Remove elements which cannot be the
+            // next greater temperature
+            while (!st.isEmpty() && temp[st.peek()] <= temp[i]) {
+                st.pop();
             }
+
+            if (st.isEmpty()) {
+                nge[i] = 0;
+            } else {
+                nge[i] = st.peek();
+            }
+
             st.push(i);
         }
+
+        for (int i = 0; i < n; i++) {
+
+            if (nge[i] == 0) {
+                ans[i] = 0;
+            } else {
+                ans[i] = nge[i] - i;
+            }
+        }
+
         return ans;
     }
 }
