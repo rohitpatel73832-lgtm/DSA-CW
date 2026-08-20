dc48541f0b96ab310243bf81fccdcc8c3e2ad68f
Rohit Patel <rohitpatel73832@gmail.com>
2026-08-20 23:02:55 +0530

    Time: 14 ms (67.84%) | Memory: 48.8 MB (23.22%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,17 +1,50 @@
+// class Solution {
+//     public int[] nextGreaterElements(int[] nums) {
+//         int n= nums.length;
+//         int[] ans= new int[n];
+//         for( int i=0; i<n ; i++){
+//             ans[i] = -1;
+//             for(int j=1;j<n; j++){
+//                 int next = nums[(i + j) % n];
+//                 if(nums[i]<next){
+//                     ans[i]=next;
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n= nums.length;
-        int[] ans= new int[n];
-        for( int i=0; i<n ; i++){
-            ans[i] = -1;
-            for(int j=1;j<n; j++){
-                int next = nums[(i + j) % n];
-                if(nums[i]<next){
-                    ans[i]=next;
-                    break;
-                }
-            }
+
+        int n = nums.length;
+        int[] nge = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        // Put circular elements into stack
+        for (int i = n - 1; i >= 0; i--) {
+            st.push(nums[i]);
         }
-        return ans;
+
+        // Find NGE
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && nums[i] >= st.peek()) {
+                st.pop();
+            }
+
+            if (st.isEmpty()) {
+                nge[i] = -1;
+            } else {
+                nge[i] = st.peek();
+            }
+
+            st.push(nums[i]);
+        }
+
+        return nge;
     }
 }
