cfd566f78cc01e5dcc384974ce60833b293b8425
Rohit Patel <rohitpatel73832@gmail.com>
2026-08-30 23:35:35 +0530

    Time: 175 ms (17.48%) | Memory: 281.1 MB (62.63%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,38 +1,75 @@
-class Solution {
-    public void bfs(int st,List<List<Integer>> ans, boolean[] vis,int dest){
-        Queue<Integer> q= new LinkedList<>();
-        q.add(st);
-        vis[st]=true;
-        while(q.size()>0){
-            int front = q.remove();
-            for(int ele: ans.get(front)){
+// class Solution {
+//     public void bfs(int st,List<List<Integer>> ans, boolean[] vis,int dest){
+//         Queue<Integer> q= new LinkedList<>();
+//         q.add(st);
+//         vis[st]=true;
+//         while(q.size()>0){
+//             int front = q.remove();
+//             for(int ele: ans.get(front)){
                 
-                if(vis[ele]==false){
+//                 if(vis[ele]==false){
+//                     q.add(ele);
+//                     vis[ele]=true;
+//                 }
+//             }
+//         }
+//     }
+//     public boolean validPath(int n, int[][] edges, int s, int dest) {
+//         int m=edges.length;
+//         List<List<Integer>> ans= new ArrayList<>();
+//         boolean[] vis= new boolean[n];
+//         for(int i=0; i<n; i++){
+//             ans.add(new ArrayList<>());
+//         }
+//         for(int i=0; i<m; i++){
+//             int a=edges[i][0];
+//             int b=edges[i][1];
+//             ans.get(a).add(b);
+//             ans.get(b).add(a);
+//         }
+
+//         for(int i=0; i<n; i++){
+//             if(!vis[i]){
+//                 bfs(s,ans,vis,dest);
+//             }
+//         }
+//         return vis[dest];
+//     }
+// }
+
+class Solution {
+    public void bfs(int source, int destination, List<List<Integer>> adj, boolean[] vis){
+        Queue<Integer> q= new LinkedList<>();
+        q.add(source);
+        vis[source]=true;
+
+        while(q.size()>0){
+            int front= q.remove();
+
+            for(int ele: adj.get(front)){
+                if(!vis[ele]){
                     q.add(ele);
                     vis[ele]=true;
                 }
             }
         }
     }
-    public boolean validPath(int n, int[][] edges, int s, int dest) {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<List<Integer>> adj= new ArrayList<>();
         int m=edges.length;
-        List<List<Integer>> ans= new ArrayList<>();
         boolean[] vis= new boolean[n];
         for(int i=0; i<n; i++){
-            ans.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
         }
         for(int i=0; i<m; i++){
-            int a=edges[i][0];
-            int b=edges[i][1];
-            ans.get(a).add(b);
-            ans.get(b).add(a);
-        }
+            int u=edges[i][0];
+            int v=edges[i][1];
 
-        for(int i=0; i<n; i++){
-            if(!vis[i]){
-                bfs(s,ans,vis,dest);
-            }
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
-        return vis[dest];
+        bfs(source,destination,adj,vis);
+
+        return vis[destination];
     }
 }
