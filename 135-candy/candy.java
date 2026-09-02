9e832cd98f99b3d3d3f9677e42eac4aa9e6e229e
Rohit Patel <rohitpatel73832@gmail.com>
2026-09-03 03:15:11 +0530

    Time: 4 ms (19.02%) | Memory: 56.1 MB (6.21%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int[] leftToRight=new int[n];
+        int[] rightToLeft= new int[n];
+        int[] ans= new int[n];
+        Arrays.fill(leftToRight,1);
+        Arrays.fill(rightToLeft,1);
+        for(int i=1; i<n; i++){
+            if(ratings[i]>ratings[i-1]){
+                leftToRight[i] = leftToRight[i - 1] + 1;
+            }
+        }
+
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i]>ratings[i+1]){
+                rightToLeft[i]=rightToLeft[i+1]+1;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            ans[i]=Math.max(leftToRight[i],rightToLeft[i]);
+        }
+
+        int count=0;
+        for(int ele:ans){
+            count+=ele;
+        }
+
+        return count;
+    }
+}
