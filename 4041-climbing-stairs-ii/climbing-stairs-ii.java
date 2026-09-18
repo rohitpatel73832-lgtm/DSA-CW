0228b5a5254c711c2ec758f933859d23a8e17c2e
Rohit Patel <rohitpatel73832@gmail.com>
2026-09-19 04:16:38 +0530

    Time: 49 ms (13.49%) | Memory: 133.4 MB (6.61%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -1,22 +1,31 @@
 class Solution {
-    public int climbStairs(int n, int[] costs) {
-        int[] dp = new int[n + 1];
-
-        dp[0] = 0;
-
-        for (int i = 1; i <= n; i++) {
-            dp[i] = Integer.MAX_VALUE;
-
-            if (i - 1 >= 0)
-                dp[i] = Math.min(dp[i], dp[i - 1] + costs[i - 1] + 1);
-
-            if (i - 2 >= 0)
-                dp[i] = Math.min(dp[i], dp[i - 2] + costs[i - 1] + 4);
-
-            if (i - 3 >= 0)
-                dp[i] = Math.min(dp[i], dp[i - 3] + costs[i - 1] + 9);
+    public int helper(int i, int n, int[] costs,int[] dp){
+        if(i==n){
+            return 0;
+        }
+        if(dp[i]!=Integer.MAX_VALUE){
+            return dp[i];
         }
 
-        return dp[n];
+        int jump=Integer.MAX_VALUE;
+        int jumpOne=Integer.MAX_VALUE;
+        int jumpTwo=Integer.MAX_VALUE;
+
+        if(i+1<=n){
+            jump=costs[i]+(i+1-i)*(i+1-i) +helper(i+1,n,costs,dp);
+        }
+        if(i+2<=n){
+            jumpOne=costs[i+1]+(i+2-i)*(i+2-i) +helper(i+2,n,costs,dp);
+        }
+        if(i+3<=n){
+            jumpTwo=costs[i+2]+(i+3-i)*(i+3-i) +helper(i+3,n,costs,dp);
+        }
+         
+        return dp[i]= Math.min(jump,Math.min(jumpOne,jumpTwo));
+    }
+    public int climbStairs(int n, int[] costs) {
+        int[] dp= new int[n];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        return helper(0,n,costs,dp);
     }
 }
