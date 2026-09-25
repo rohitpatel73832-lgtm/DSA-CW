ec156210c8ab43f48a6801aac132a03c8305ad49
Rohit Patel <rohitpatel73832@gmail.com>
2026-09-25 19:36:29 +0530

    Time: 136 ms (5.02%) | Memory: 122.6 MB (6.41%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,31 +1,70 @@
+// class Solution {
+//     public boolean subset(int i,int[] arr,int target,int[][] dp){
+//         if(i==arr.length){
+//             if(target==0) return true;
+//             else return false;
+//         }
+//         if(dp[i][target]!=-1) return (dp[i][target]==1);
+//         boolean ans = false;
+//         boolean skip = subset(i+1,arr,target,dp);
+//         if(target-arr[i]<0) ans = skip;
+//         else{
+//             boolean pick = subset(i+1,arr,target-arr[i],dp);
+//             ans=pick||skip;
+//         }
+//         dp[i][target]=(ans)?1:0;
+//         return ans;
+//     }
+//     public boolean canPartition(int[] nums) {
+//         int sum=0;
+//         for(int ele:nums) sum+=ele;
+//         if(sum%2!=0) return false;
+//         int target=sum/2;
+//         int[][] dp=new int[nums.length][target+1];
+//         for(int i=0; i<dp.length;i++){
+//             for(int j=0;j<dp[0].length;j++){
+//                 dp[i][j]=-1;
+//             }
+//         } 
+//         return subset(0,nums,target,dp);
+//     }
+// }
+
 class Solution {
-    public boolean subset(int i,int[] arr,int target,int[][] dp){
-        if(i==arr.length){
-            if(target==0) return true;
-            else return false;
+    //boolean ans;
+    public boolean helper(int[] nums,int i,int sum,int count,Boolean[][] dp){
+        if(count == sum/2){
+            //ans = true;
+            return true;
         }
-        if(dp[i][target]!=-1) return (dp[i][target]==1);
-        boolean ans = false;
-        boolean skip = subset(i+1,arr,target,dp);
-        if(target-arr[i]<0) ans = skip;
-        else{
-            boolean pick = subset(i+1,arr,target-arr[i],dp);
-            ans=pick||skip;
+        if(i>=nums.length){
+            return false;
         }
-        dp[i][target]=(ans)?1:0;
-        return ans;
+
+        if(dp[i][count]!=null){
+            return dp[i][count];
+        }
+        
+
+        // helper(nums,i+1,sum,count+nums[i]);
+        // helper(nums,i+1,sum,count);
+
+        boolean take=helper(nums,i+1,sum,count+nums[i],dp);
+        boolean skip=helper(nums,i+1,sum,count,dp);
+
+        return dp[i][count] = take || skip;
+
     }
     public boolean canPartition(int[] nums) {
         int sum=0;
-        for(int ele:nums) sum+=ele;
-        if(sum%2!=0) return false;
-        int target=sum/2;
-        int[][] dp=new int[nums.length][target+1];
-        for(int i=0; i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j]=-1;
-            }
-        } 
-        return subset(0,nums,target,dp);
+        for(int ele:nums){
+            sum+=ele;
+        }
+        if(sum % 2 != 0){
+            return false;
+        }
+        Boolean[][] dp= new Boolean[nums.length][sum];
+        return helper(nums,0,sum,0,dp);
+        //return ans;
     }
 }
